2cf5316de576b8c4c796f6429c364d3acd35d45f
lucasrosa21 <134444831+lucasrosa21@users.noreply.github.com>
2023-12-14 18:05:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+
+
+let boxBuscar = document.querySelector('.buscar-box');
+let lupa = document.querySelector('.lupa-buscar');
+let btnFechar = document.querySelector('.btn-fechar');
+
+lupa.addEventListener('click', ()=> {
+    boxBuscar.classList.add('ativar')
+})
+
+btnFechar.addEventListener('click', ()=> {
+    boxBuscar.classList.remove('ativar')
+})
+
+
+
+
+
